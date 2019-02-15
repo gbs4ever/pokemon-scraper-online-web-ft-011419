338376b8f584b41eb73456a5423cf36bb838b310
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-15 05:36:11 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,7 +11,7 @@ def initialize ( id:, name:, type:, db:nil, hp:60)
 end
 
 
-def self.save(name, type, db,hp=60)
+def self.save(name, type, hp=60,db)
 db.execute("INSERT INTO Pokemon  (name, type,hp) VALUES (?, ?,?)",name, type,hp)
 end
 
